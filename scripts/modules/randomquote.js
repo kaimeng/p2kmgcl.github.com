4f441e1d905dedf73e1725064abe83b406b8d697
Pablo Molina <p2kmgcl@gmail.com>
2013-05-21 12:05:21 +0200

    - Añadida la posibilidad de usar o no lesscss (en producción no se usará). - Ahora los módulos de javascript están el jsmodules (fichero _config.yml) - Tema LoremIpsum:   - Modificado el tamaño de la barra superior (agrandado).   - Añadidos estilos para la carga de nuevas entradas (waypoint-infinite)

--- scripts/modules/randomquote.js
+++ scripts/modules/randomquote.js
@@ -4,9 +4,12 @@
 p2kmgcl.modules.randomQuote = function () {
     $('.randomQuotes > ul').each(function () {
         var $li = $(this).find(' > .quoteWrapper'),
+            $old = $(this).find(' > .quoteWrapper.chosen'),
             rand = parseInt(Math.random() * $li.size());
 
+        // Si habia notas esconde la vieja y muestra la nueva
         if ($li.size() > 0) {
+            $old.removeClass('chosen');
             $($li[rand]).addClass('chosen');
         }
     });
