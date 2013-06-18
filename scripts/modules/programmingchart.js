bc05ba4f7bc1b78eca714de3f9bb5df95bb32cec
Pablo Molina <p2kmgcl@gmail.com>
2013-06-18 11:02:59 +0200

    Actualizado el tamaño del gráfico de programación (se redimensiona) y aumentado ligeramente el ancho de las etiquetas.

--- scripts/modules/programmingchart.js
+++ scripts/modules/programmingchart.js
@@ -6,43 +6,51 @@ p2kmgcl.modules.programmingChart = function () {
     var $curriculum = $(".page.curriculum"),
         $programming = $curriculum.find("> .programming"),
         $valueList = $programming.find("> ul"),
-        $valueElements = $valueList.find("> li");
+        $valueElements = $valueList.find("> li"),
 
-    if ($valueList.size() > 0) {
-        var canvas = document.createElement('canvas'),
-            $canvas = $(canvas),
-            elementHeight = parseInt($valueList.css('font-size')) * 2,
-            elementLowHeight = elementHeight - 5;
-        canvas.width = $curriculum.width() * 0.75;
-        canvas.height = $valueElements.size() * elementHeight;
-        $programming.append(canvas);
+        canvas = document.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        $canvas = $(canvas),
+        colors = [];
 
-        var ctx = canvas.getContext('2d'),
-            maxWidth = canvas.width - 10,
+    $programming.append(canvas);
 
-            /**
-             * Genera un color aleatorio.
-             * Origen: http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
-             */
-            getRandomColor = function () {
-                var letters = '0123456789ABCDEF'.split(''),
-                    color = '#';
-                for (var i = 0; i < 6; i++ ) {
-                    color += letters[Math.round(Math.random() * 15)];
-                }
-                return color;
-            };
+    $(window).on('resize', (function () {
+        if ($valueList.size() > 0) {
+            var elementHeight = parseInt($valueList.css('font-size')) * 2,
+                elementLowHeight = elementHeight - 5;
 
-        $valueElements.each(function (index) {
-            var color = getRandomColor();
+            canvas.width = $curriculum.width() * 0.65;
+            canvas.height = $valueElements.size() * elementHeight;
 
-            ctx.beginPath();
-            ctx.moveTo(0, index * elementHeight + 5);
-            ctx.rect(0, index * elementHeight + 5, maxWidth * parseFloat($(this).attr('data-level')) / 10, elementLowHeight);
-            ctx.closePath();
+            var maxWidth = canvas.width - 10,
 
-            ctx.fillStyle = color;
-            ctx.fill();
-        });
-    }
+                /**
+                 * Genera un color aleatorio.
+                 * Origen: http://stackoverflow.com/questions/1484506/random-color-generator-in-javascript
+                 */
+                getRandomColor = function () {
+                    var letters = '0123456789ABCDEF'.split(''),
+                        color = '#';
+                    for (var i = 0; i < 6; i++ ) {
+                        color += letters[Math.round(Math.random() * 15)];
+                    }
+                    return color;
+                };
+
+            $valueElements.each(function (index) {
+                var color = colors[index] || getRandomColor();
+                colors[index] = color;
+
+                ctx.beginPath();
+                ctx.moveTo(0, index * elementHeight + 5);
+                ctx.rect(0, index * elementHeight + 5, maxWidth * parseFloat($(this).attr('data-level')) / 10, elementLowHeight);
+                ctx.closePath();
+
+                ctx.fillStyle = color;
+                ctx.fill();
+            });
+        }
+        return arguments.callee;
+    }()));
 };
