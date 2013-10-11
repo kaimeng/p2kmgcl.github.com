d0d692dd62d3bdc226138c9961cb79b8445bb003
Pablo Molina <p2kmgcl@gmail.com>
2013-10-11 03:14:46 -0400

    Actualizadas las librerías y guardado el código no minimizado por si se necesita depurar

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 ---
 ---
 
-if (typeof less == 'undefined') { less = { env: "{{ site.less.env }}" }; {% include scripts/libs/less/less-1.3.3.min.js %} }
+if (typeof less == 'undefined') { less = { env: "{{ site.less.env }}" }; {% include scripts/libs/less/less-1.4.1.min.js %} }
 {% include scripts/libs/modernizr/modernizr-2.6.2-custom.min.js %}
-{% include scripts/libs/prefixfree/prefixfree-1.0.3.min.js %}
-if (typeof $ == 'undefined') { {% include scripts/libs/jquery/jquery-1.9.1.min.js %} }
-{% include scripts/libs/jquery_waypoints/waypoints-2.0.2.min.js %}
+{% include scripts/libs/prefixfree/prefixfree-1.0.7.min.js %}
+if (typeof $ == 'undefined') { {% include scripts/libs/jquery/jquery-1.10.2.min.js %} }
+{% include scripts/libs/jquery_waypoints/waypoints-2.0.3.min.js %}
 
 /**
  * Crea el objeto global con toda la configuración de la página
@@ -16,16 +16,16 @@ window.p2kmgcl = new function () {
         _proto = this.constructor.prototype,
         _modules = {
             //infiniteScroll:   {% comment %}{% include scripts/modules/infinitescroll.js %}{% endcomment %},
-            embedComment:       {% include scripts/modules/embedcomments.js %},
-            introWaypoint:      {% include scripts/modules/introwaypoint.js %},
-            programmingChart:   {% include scripts/modules/programmingchart.js %},
-            randomQuote:        {% include scripts/modules/randomquote.js %},
-            showGoTop:          {% include scripts/modules/showgotop.js %},
-            localStorage:       {% include scripts/modules/localstorage.js %}
+            embedComment:       {% include scripts/modules/minified/embedcomments.min.js %},
+            introWaypoint:      {% include scripts/modules/minified/introwaypoint.min.js %},
+            programmingChart:   {% include scripts/modules/minified/programmingchart.min.js %},
+            randomQuote:        {% include scripts/modules/minified/randomquote.min.js %},
+            showGoTop:          {% include scripts/modules/minified/showgotop.min.js %},
+            localStorage:       {% include scripts/modules/minified/localstorage.min.js %}
         };
 
     // Crea el método de carga de módulos
-    _proto.module = {% include scripts/modules/_main.js %};
+    _proto.module = {% include scripts/modules/minified/main.min.js %};
     return this;
 };
 
