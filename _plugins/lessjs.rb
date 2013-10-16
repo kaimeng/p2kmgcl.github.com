1e5512b92ca9b39b775b08a9f10ccdbfeda9108f
Pablo Molina <p2kmgcl@gmail.com>
2013-10-16 10:52:53 -0400

    Se ha corregido varios fallos en la generación del contenido y se ha adaptado el script de compilación para generar los nuevos temas.

--- _plugins/lessjs.rb
+++ _plugins/lessjs.rb
@@ -10,29 +10,25 @@ module Jekyll
   class LessJsGenerator < Generator
     safe true
     priority :low
-    
+
     def generate(site)
-      less_themes     = site.config['less']['themeList']
-      less_themePath  = site.config['less']['themePath'] + '/'
-      less_cssPath    = site.config['less']['themePath'] + '/'
-      less_bin        = site.config['less']['bin'] || 'lessc'
-      
-      for less_theme in less_themes
-        FileUtils.mkdir_p(less_cssPath)
-          begin
-            command = [less_bin, ' -x ',
-                       less_themePath + less_theme + '/index.less',
-                       ' > ',
-                       less_cssPath + less_theme + '.css'
-                      ].join(' ')
-            
-            puts 'Compiling LESS: ' + command
-            puts `#{command}`
-            raise "LESS compilation error" if $?.to_i != 0
-          end
-          
-          # Add this output file so it won't be "cleaned away"
-          site.static_files << CssFile.new(site, site.source, less_cssPath, less_theme + '.css')
+      themes    = site.config['less']['themeList']
+      themePath = site.config['less']['themePath'] + '/'
+      modules   = site.config['less']['themeModules']
+      bin       = site.config['less']['bin'] + ' -x '
+
+      puts ''
+      for theme in themes
+        curLess = themePath + theme + '/build_less/';
+        curCSS = themePath + theme + '/build_css/';
+        printf "\nCompiling theme " + theme + "\n:"
+        for themeModule in modules
+          command = bin + curLess + themeModule + '.less > ' + curCSS + themeModule + '.css'
+          printf "\tGenerating " + themeModule + ' module. ' + `#{command}` + "\n"
+          raise "LESS compilation error" if $?.to_i != 0
+          site.static_files << CssFile.new(site, site.source, curCSS, themeModule + '.css')
+        end
+        printf "\tdone."
       end
     end
   end
